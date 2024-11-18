e01e1b88e22d1e623ff3cfde0c5e204ba464f025
wambuiGicheha <keziahgicheha@gmail.com>
2024-11-19 00:31:34 +0300

    app UI/UX

--- app.py
+++ app.py
@@ -17,11 +17,24 @@ pipeline = joblib.load('models/sentiments_pipeline.pkl')
 # Streamlit app configuration
 st.set_page_config(page_title="Mental Well-Being Screening", page_icon="🧠", layout="wide")
 
+# Set blue background color using CSS
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #e0f7fa; /* Light blue shade */
+        opacity: 0.3;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Title and description
 st.title("Mental Well-Being Screening Tool 🧠")
 st.write("""
 This application analyzes social media posts from Reddit to predict whether a user is likely experiencing depression or not. It leverages 
-         a pre-trained machine learning model for advanced text analysis
+         a pre-trained machine learning model for advanced sentiment analysis
 """)
 
 # Input section
@@ -29,7 +42,7 @@ st.header("Enter Reddit Text for Analysis")
 user_input = st.text_area(
     "Paste your text here (e.g., Reddit title entry or Reddit body entry):",
     placeholder="Type or paste text here...",
-    height=150
+    height=100
 )
 
 # Button to make predictions
@@ -58,6 +71,18 @@ from text from Reddit posts.
 It utilizes **TF-IDF** for text vectorization and a logistic regression model for classification.
 """)
 st.sidebar.write("The dataset used for training includes labeled social media posts.")
+
+
+st.markdown("---")  # Horizontal divider
+st.markdown(
+    """
+    <div style='text-align: center; font-size: 0.9em; color: gray;'>
+        <strong>Disclaimer:</strong> Disclaimer: This tool is designed to flag posts that may indicate potential signs of depression. It is not a diagnostic tool. 
+        For professional help, please consult a licensed mental health professional or a trusted support service.
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # Footer
 st.write("---")
